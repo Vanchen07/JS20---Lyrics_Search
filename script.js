f95bb3b76357f616bcef1a6396354bac1309fcb2
Vanessa Chen <vanchen07@gmail.com>
2020-06-12 11:48:36 -0700

    grab dom elements and add event listener to form

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const result = document.getElementById('result');
+const more = document.getElementById('more');
+
+const apiURL = 'https://api.lyrics.ovh';
+
+form = addEventListener('submit', e => {
+    e.preventDefault();
+
+    const searchTerm = search.value.trim();
+
+    if (!searchTerm) {
+        alert('Please type in a search term');
+    } else {
+        searchSongs(searchTerm);
+    }
+});
